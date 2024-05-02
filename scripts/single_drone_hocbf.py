510558b9fc5e84f18e575171e785a882b7677f18
DS3a <33351122+DS3a@users.noreply.github.com>
2024-05-02 11:59:33 +0530

    it worked once

--- scripts/single_drone_hocbf.py
+++ scripts/single_drone_hocbf.py
@@ -11,7 +11,7 @@ from multi_drone_control.msg import attitude_thrust_cmd
 from scipy.spatial.transform import Rotation
 
 # Constants
-mass_drone = 0.68
+mass_drone = 0.825
 mass_rotor = 0.009
 
 m_T = mass_drone + 4*mass_rotor
@@ -57,9 +57,9 @@ g = sp.Matrix([
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
+    [1/m_T, 0, 0],
+    [0, 1/m_T, 0],
+    [0, 0, 1/m_T],
 ])
 
 
@@ -77,20 +77,34 @@ def Lf(barrier_fn, order=1):
 
 
 t = sp.symbols("t")  # symbol for time
-dist = (sp.Matrix([p_x, p_y, p_z]) - sp.Matrix([-2, 2, 2]))
-b1 = (50.1 - (50/14)*t) - dist.dot(dist)
-b1 = 8*sp.exp(-t/3) + 0.1 - dist.dot(dist)
-b2 = (347.93*sp.exp(-0.418*t) + 2) - dist.dot(dist)
-b = -sp.ln(sp.exp(b1) + sp.exp(b2))
-b = b1
+dist = ((sp.Matrix([p_x, p_y, p_z]) - sp.Matrix([2, 2, 2])))
+# b1 = (50.1 - (50/14)*t) - sp.sqrt(dist.dot(dist))
+b1 = 3*sp.exp(-t/18) + 0.1 - sp.sqrt(dist.dot(dist))
+b2 = (347.93*sp.exp(-0.418*t) + 2) - sp.sqrt(dist.dot(dist))
+b = -sp.ln(sp.exp(-b1) + sp.exp(-b2))
+# b = b1
 
 # barrier functions to enforce velocity constraints
-nu_max = 0.3  # max amplitude of pitch or roll can be 0.3 radians
-b_pitch = (v_z * np.tan(nu_max))**2 - v_x**2
-b_roll = (v_z * np.tan(nu_max))**2 - v_y**2
+nu_max = 0.8  # max amplitude of pitch or roll can be 0.3 radians
+# b_pitch_max = nu_max - sp.atan(v_x / v_z)
+# b_pitch_min = nu_max + sp.atan(v_x / v_z)
 
-v_max = 1.5  # max v_z can be 0.5 this constraint trickles down to v_x and v_y which have to be lesser than some ratio of v_z at all times
-b_vz = (v_max**2) - v_z**2
+b_pitch_max = nu_max - v_x
+b_pitch_min = nu_max + v_x
+
+
+# b_roll_max = nu_max - sp.atan(v_y / v_z)
+# b_roll_min = nu_max + sp.atan(v_y / v_z)
+
+
+b_roll_max = nu_max - v_y
+b_roll_min = nu_max + v_y
+
+v_max = 15.5
+v_min = -5.0
+b_v_z_max = v_max - v_z
+b_v_z_min = -v_min + v_z
+
 
 # Variables to keep track of states
 pos_x = None
@@ -131,67 +145,123 @@ def main():
     rate = rospy.Rate(50)  # 50hz
 
     taken_off = False
-    M = np.array([[1.0, 2.0, 0.0], [-8.0, 3.0, 2.0], [0.0, 1.0, 1.0]],
-                 dtype=float)
-    P = M.T @ M  # positive semi definite matrix
-    P = 0.5*np.eye(3)
+    received_initial_solution = False
+    a = 0
+
+    P = np.eye(3)
     Q = np.array([0., 0., 0.], dtype=float).reshape((3,))
-    start = time.time()
 
     alpha = 0.9
     lhs_ax = -lie_derivative(lie_derivative(b, f, 1), g.col(0))
-    lhs_az = -lie_derivative(lie_derivative(b, f, 1), g.col(2))
     lhs_ay = -lie_derivative(lie_derivative(b, f, 1), g.col(1))
-    rhs = Lf(b, 2) + sp.diff(b, t, 2) + alpha*sp.diff(b, t) + alpha*alpha*b + Lf(alpha*b) + sp.diff(alpha*b, t)
+    lhs_az = -lie_derivative(lie_derivative(b, f, 1), g.col(2))
+    # rhs = Lf(b, 2) + sp.diff(b, t, 2) + alpha*sp.diff(b, t) + alpha*alpha*b + Lf(alpha*b) + sp.diff(alpha*b, t)
+    rhs = Lf(b, 2) + sp.diff(b, t, 2) + 2*alpha*Lf(b) + alpha*sp.diff(b, t) + (alpha**2)*b
+    # rhs = Lf(b, 2) + sp.diff(b, t, 2) + 2*b*Lf(b) + 2*b*sp.diff(b, t) + (Lf(b))**2 + b**4 + 2*(b**2)*Lf(b)
+    # rhs = Lf(b, 2) + sp.diff(b, t, 2) + sp.diff(b, t)**2 + (alpha*b)**2 + Lf(alpha*b) + sp.diff(alpha*b, t)
+    # rhs = Lf(b, 2) + sp.diff(b, t, 2) + sp.diff(b, t)**2 + b**4 + Lf(b**2) + sp.diff(b**2, t)
+    # rhs = Lf(b, 2) + sp.diff(b, t, 2) + 2*b*Lf(b) + 2*b*sp.diff(b, t) + Lf(b)**2 + sp.diff(b, t)**2 + b**4 + 2*Lf(b)*sp.diff(b, t) + 2*(b**2)*Lf(b) + 2*(b**2)*sp.diff(b, t)
+    # rhs = Lf(b, 2) + 2*b*Lf(b) + Lf(b)**2 + 2*(b**2)*Lf(b)+b**4
 
-    lhs_b_pitch_ax = -lie_derivative(b_pitch, g.col(0))
-    lhs_b_pitch_ay = 0
-    lhs_b_pitch_az = -lie_derivative(b_pitch, g.col(2))
-    rhs_b_pitch = lie_derivative(b_pitch, f) + alpha*b_pitch
+    alpha = 0.3
+    pow = 1
+    lhs_b_pitch_min_ax = -lie_derivative(b_pitch_min, g.col(0))
+    lhs_b_pitch_min_ay = 0
+    lhs_b_pitch_min_az = -lie_derivative(b_pitch_min, g.col(2))
+    # rhs_b_pitch_min = lie_derivative(b_pitch_min, f) + alpha*b_pitch_min
+    rhs_b_pitch_min = lie_derivative(b_pitch_min, f) + alpha*b_pitch_min**pow
 
-    lhs_b_roll_ax = 0
-    lhs_b_roll_ay = -lie_derivative(b_roll, g.col(1))
-    lhs_b_roll_az = -lie_derivative(b_roll, g.col(2))
-    rhs_b_roll = lie_derivative(b_roll, f) + alpha*b_roll
+    lhs_b_pitch_max_ax = -lie_derivative(b_pitch_max, g.col(0))
+    lhs_b_pitch_max_ay = 0
+    lhs_b_pitch_max_az = -lie_derivative(b_pitch_max, g.col(2))
+    # rhs_b_pitch_max = lie_derivative(b_pitch_max, f) + alpha*b_pitch_max
+    rhs_b_pitch_max = lie_derivative(b_pitch_max, f) + alpha*b_pitch_max**pow
 
-    lhs_b_vz_az = -lie_derivative(b_vz, g.col(2))
-    rhs_b_vz = lie_derivative(b_vz, f) + alpha*b_vz
+    lhs_b_roll_min_ax = 0
+    lhs_b_roll_min_ay = -lie_derivative(b_roll_min, g.col(1))
+    lhs_b_roll_min_az = -lie_derivative(b_roll_min, g.col(2))
+    # rhs_b_roll_min = lie_derivative(b_roll_min, f) + alpha*b_roll_min
+    rhs_b_roll_min = lie_derivative(b_roll_min, f) + alpha*b_roll_min**pow
 
+    lhs_b_roll_max_ax = 0
+    lhs_b_roll_max_ay = -lie_derivative(b_roll_max, g.col(1))
+    lhs_b_roll_max_az = -lie_derivative(b_roll_max, g.col(2))
+    # rhs_b_roll_max = lie_derivative(b_roll_max, f) + alpha*b_roll_max
+    rhs_b_roll_max = lie_derivative(b_roll_max, f) + alpha*b_roll_max**pow
+
+    lhs_b_v_z_max_az = -lie_derivative(b_v_z_max, g.col(2))
+    # rhs_b_v_z_max = lie_derivative(b_v_z_max, f) + alpha * b_v_z_max
+    rhs_b_v_z_max = lie_derivative(b_v_z_max, f) + alpha*b_v_z_max**pow
+
+    lhs_b_v_z_min_az = -lie_derivative(b_v_z_min, g.col(2))
+    # rhs_b_v_z_min = lie_derivative(b_v_z_min, f) + alpha * b_v_z_min
+    rhs_b_v_z_min = lie_derivative(b_v_z_min, f) + alpha*b_v_z_min**pow
+
+    start = time.time()
     while not rospy.is_shutdown():
         if not taken_off:
             print("taking off")
             take_off_msg = attitude_thrust_cmd()
-            take_off_msg.thrust.data = 7.9
+            take_off_msg.thrust.data = 7.5
             take_off_msg.roll.data = 0.0
             take_off_msg.pitch.data = 0.0
             take_off_msg.yaw.data = 0.0
             pub.publish(take_off_msg)
-            if pos_z >= 1.00:
-                f_T_real = 5.5
-                take_off_msg.thrust.data = f_T_real
-                pub.publish(take_off_msg)
+            if pos_z >= 0.25:
                 taken_off = True
+                start = time.time()
             rate.sleep()
             continue
 
         time_now = time.time() - start
-        G = np.array([[eval_expr(lhs_ax, time_now), eval_expr(lhs_ay, time_now), eval_expr(lhs_az, time_now)],  # HOCBF constraints
-                      [eval_expr(lhs_b_roll_ax), eval_expr(lhs_b_roll_ay), eval_expr(lhs_b_roll_az)],  # roll angle constraint
-                      [eval_expr(lhs_b_pitch_ax), eval_expr(lhs_b_pitch_ay), eval_expr(lhs_b_pitch_az)], #], dtype=float)  # pitch angle constraint
-                      # [0, 0, eval_expr(lhs_b_v_z_max_az)],  # v_z_max constraint
-                      [0, 0, eval_expr(lhs_b_vz_az)]], dtype=float)  # v_z min constraint
+        G = np.array([[eval_expr(lhs_ax, time_now), eval_expr(lhs_ay, time_now), eval_expr(lhs_az, time_now)],  #  ], dtype=float) # HOCBF constraints
+                      [eval_expr(lhs_b_roll_max_ax), eval_expr(lhs_b_roll_max_ay), eval_expr(lhs_b_roll_max_az)],  # roll angle max constraint
+                      [eval_expr(lhs_b_roll_min_ax), eval_expr(lhs_b_roll_min_ay), eval_expr(lhs_b_roll_min_az)],  # roll angle min constraint
+                      [eval_expr(lhs_b_pitch_max_ax), eval_expr(lhs_b_pitch_max_ay), eval_expr(lhs_b_pitch_max_az)], #], dtype=float)  # pitch angle max constraint
+                      [eval_expr(lhs_b_pitch_min_ax), eval_expr(lhs_b_pitch_min_ay), eval_expr(lhs_b_pitch_min_az)], #], dtype=float)  # pitch angle max constraint
+                      [0, 0, eval_expr(lhs_b_v_z_max_az)],  # v_z_max constraint
+                      [0, 0, eval_expr(lhs_b_v_z_min_az)]], dtype=float)  # v_z min constraint
 
-        h = np.array([[eval_expr(rhs, time_now)],  # HOCBF constraint
-                      [eval_expr(rhs_b_roll)],  # roll angle constraint
-                      [eval_expr(rhs_b_pitch)], #], dtype=float)  # pitch angle constraint
-                      # [eval_expr(rhs_b_v_z_max)],  # v_z_max constraint
-                      [eval_expr(rhs_b_vz)]], dtype=float)  # v_z_min constraint
+        print("rhs of hocbf is ", eval_expr(rhs, time_now))
+
+        delta = 0.5
+        h = np.array([[eval_expr(rhs, time_now)+2],    #  ], dtype=float)  # HOCBF constraint
+                      [eval_expr(rhs_b_roll_max)+delta],  # roll angle constraint
+                      [eval_expr(rhs_b_roll_min)+delta],  # roll angle constraint
+                      [eval_expr(rhs_b_pitch_max)+delta],  # ], dtype=float)  # pitch angle constraint
+                      [eval_expr(rhs_b_pitch_min)+delta],  # ], dtype=float)  # pitch angle constraint
+                      [eval_expr(rhs_b_v_z_max)],  # v_z_max constraint
+                      [eval_expr(rhs_b_v_z_min)]], dtype=float)  # v_z_min constraint
 
         sol = solvers.qp(matrix(P), matrix(Q), matrix(G), matrix(h))
 
-        a = np.array(sol['x'])  # the acceleration vector
-        a = a*m_T  # convert this to forces
+        lpf_const = 0.1
+        if received_initial_solution:
+            a = (lpf_const * a) + (1 - lpf_const)*np.array(sol['x'])
+        else:
+            a = np.array(sol['x'])  # the acceleration vector
+
+        if a[2] < 0:
+            a[2] = 0
+        # elif a[2] > 1:
+        #     a[2] = 1
+
+        # if a[0] < -1:
+        #     a[0] = -1
+        # elif a[0] > 1:
+        #     a[0] = 1
+
+        # if a[1] < -1:
+        #     a[1] = -1
+        # elif a[1] > 1:
+        #     a[1] = 1
+
+
+
+        # a = a*m_T  # convert this to forces
         f_t = np.linalg.norm(a)  # this is the thrust force requried
+        if f_t > 7.3:
+            f_t = 7.3
         a_cap = (a / np.linalg.norm(a)).reshape(3)  # the direction vector of a
         f_Z = np.array([0, 0, 1])
         v = np.cross(f_Z.reshape(3), a_cap.reshape(3))  # f_z x a
